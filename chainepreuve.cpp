bb5dfe1db6bf6d325053f90dfe24794354c4c6e5
ThibautRoperch <thibaut.roperch@free.fr>
2016-04-27 16:06:49 +0200

    Ancien travail importé

---
+++ chainepreuve.cpp
@@ -0,0 +1,12 @@
+#include "chainepreuve.h"
+
+using namespace std;
+
+ChainePreuve::ChainePreuve(string const & ligne)
+{
+	// séparer les lignes apres avoir remplacé les "  " par des " "
+	// push
+}
+
+ChainePreuve::~ChainePreuve()
+{}

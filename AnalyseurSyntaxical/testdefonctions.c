d0a7543429fcdb0779fe885dc32077e112e16240
ThibautRoperch <thibaut.roperch@free.fr>
2016-04-28 13:49:07 +0200

    Tests de fonctions sur cdc

--- AnalyseurSyntaxical/testdefonctions.c
+++ AnalyseurSyntaxical/testdefonctions.c
@@ -1,4 +1,4 @@
-#include "fonctions.c"
+#include "fonsouschaineions.c"
 #include <string.h>
 #include <stdio.h>
 
@@ -13,3 +13,39 @@ int main()
     printf("%s\n", ch);
     return 0;
 }
+
+
+/*
+int position(const char *chaine, const char *souschaine)
+{
+    int index = -1;
+    char *premiere_occurrence = NULL;
+    premiere_occurrence = strstr (chaine, souschaine);
+    
+    if (premiere_occurrence != NULL)
+    {
+        index = premiere_occurrence - chaine;
+    }
+
+    return index;
+}
+
+char* remplacer(char* chaine, const char* souschaine1, const char* souschaine2)
+{
+    int x = position(chaine, souschaine1);
+    char * res = NULL;
+    res = malloc(sizeof(*res) * (strlen(chaine)*strlen(souschaine2)));
+    char * tmp = NULL;
+    
+    while(x > -1) {
+        tmp = malloc(sizeof(*res) * (strlen(chaine)*strlen(souschaine2)));
+        // copier dans tmp la partie de chaine qui va de 0 à la première occurrence de souschaine1
+        strncpy(tmp, chaine, x);
+        // copier dans tmp la souschaine de remplacement
+        strcpy(tmp, souschaine2);
+        x = position(chaine, souschaine1);
+        // remplacer ?
+    }
+    return tmp;
+}
+*/

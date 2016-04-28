0211511ca5f336d215e40902ceaca586647fc42b
PierreGranier <piergranier@laposte.net>
2016-04-28 14:58:19 +0200

    fonction str_remplace correc

--- AnalyseurSyntaxical/fonctions.c
+++ AnalyseurSyntaxical/fonctions.c
@@ -1,5 +1,5 @@
 // Retourne vrai si les deux chaines sont équivalentes, faux sinon
-boolean sont_egales(char* chaine1, char* chaine2)
+/*boolean sont_egales(char* chaine1, char* chaine2)
 {
 	return !strcmp(chaine1, chaine2);
 }
@@ -53,4 +53,43 @@ char* remplacer(char* chaine, const char* souschaine1, const char* souschaine2)
 	} while(i < strlen(chaine));
 	strcpy(chaine, res);
 	return chaine;
+}*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+char* str_remplace (const char *s, unsigned int start, unsigned int lenght, const char *ct)
+{
+   char *new_s = NULL;
+
+   if (s != NULL && ct != NULL && start >= 0 && lenght > 0)
+   {
+      size_t size = strlen (s);
+
+      new_s = malloc (sizeof (*new_s) * (size - lenght + strlen (ct) + 1));
+      if (new_s != NULL)
+      {
+         memcpy (new_s, s, start);
+         memcpy (&new_s[start], ct, strlen (ct));
+         memcpy (&new_s[start + strlen (ct)], &s[start + lenght], size - lenght - start + 1);
+      }
+   }
+   else
+   {
+      fprintf (stderr, "Memoire insuffisante\n");
+      exit (EXIT_FAILURE);
+   }
+   return new_s;
+}
+
+//virer le main + les includes
+
+int main()
+{
+    char ch[] = "Bonjour les potes";
+	printf("Avant replace : %s\n", ch);
+    char *new = str_remplace(ch, 0, 7, "Salut");
+    printf("Après replace : %s\n", new);
+    return 0;
 }

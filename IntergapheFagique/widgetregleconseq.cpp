5b81b6c26bf5ba0ce8d122baeb03cdd58c5868fa
PierreGranier <piergranier@laposte.net>
2016-05-12 16:50:03 +0200

    WidgetRegleConseq.h/cpp

---
+++ IntergapheFagique/widgetregleconseq.cpp
@@ -0,0 +1,24 @@
+#include "widgetregleconseq.h"
+
+using namespace std;
+
+WidgetRegleConseq::WidgetRegleConseq(QWidget *parent) : WidgetRegle(parent)
+{
+    m_nom->setText("CONSEQ");
+	
+	m_premisse = new QLineEdit(m_top);
+	
+	m_grid->addWidget(m_premisse, 0, 1);
+	
+}
+
+WidgetRegleConseq::~WidgetRegleConseq()
+{
+    // delete m_top;
+}
+
+string WidgetRegleConseq::toString()
+{
+	return "";
+	// return le tostring des LineEdit
+}
---
+++ IntergapheFagique/widgetregleconseq.h
@@ -0,0 +1,23 @@
+#ifndef widgetregleconseq_h
+#define widgetregleconseq_h
+
+#include "widgetregle.h"
+
+class WidgetRegleConseq : public WidgetRegle 
+{
+    private:
+    	
+        QLineEdit *m_premisse;
+		
+		WidgetRegle *m_regle;
+        
+        WidgetAjouter *m_ajouter;
+        
+    public:
+        WidgetRegleConseq(QWidget *parent);
+        ~WidgetRegleConseq();
+        
+        std::string toString();
+};
+
+#endif

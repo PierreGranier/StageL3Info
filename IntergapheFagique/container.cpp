7a1c516c4c3599ecca4e11259f4d7ec9f3fcc472
ThibautRoperch <thibaut.roperch@free.fr>
2016-05-12 18:15:14 +0200

    Changement des parents des widgets

--- IntergapheFagique/container.cpp
+++ IntergapheFagique/container.cpp
@@ -7,20 +7,20 @@ Container::Container(QWidget *parent) : QWidget(parent)
 	
 	QWidget *m_top = new QWidget(this);
 	
-	QHBoxLayout *ens = new QHBoxLayout(m_top);
+	QHBoxLayout *ens = new QHBoxLayout(this);
 	
-	WidgetAjouter *afficher = new WidgetAjouter(m_top);
+	WidgetAjouter *afficher = new WidgetAjouter(this);
 	
 	ens->addWidget(afficher);
-	ens->setContentsMargins(50,50,50,50);
+	//ens->setContentsMargins(50,50,50,50);
 	// getzfzeffze - size et tout
 	
-	WidgetRegleAff *aff = new WidgetRegleAff(m_top);
-	WidgetRegleSeq *seq = new WidgetRegleSeq(m_top);
-	WidgetRegleCond *cond = new WidgetRegleCond(m_top);
-	WidgetRegleConseq *conseq = new WidgetRegleConseq(m_top);
-	WidgetRegleWhile *whileN = new WidgetRegleWhile(m_top);
-	WidgetRegleWhileT *whileT = new WidgetRegleWhileT(m_top);
+	WidgetRegleAff *aff = new WidgetRegleAff(this);
+	WidgetRegleSeq *seq = new WidgetRegleSeq(this);
+	WidgetRegleCond *cond = new WidgetRegleCond(this);
+	WidgetRegleConseq *conseq = new WidgetRegleConseq(this);
+	WidgetRegleWhile *whileN = new WidgetRegleWhile(this);
+	WidgetRegleWhileT *whileT = new WidgetRegleWhileT(this);
 	ens->addWidget(aff);
 	ens->addWidget(seq);
 	ens->addWidget(cond);

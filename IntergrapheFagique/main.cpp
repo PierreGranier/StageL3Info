3ddc4e6f259f6b78395edb288c8610852e762d9e
PierreGranier <piergranier@laposte.net>
2016-05-17 18:42:03 +0200

    TODO

--- IntergrapheFagique/main.cpp
+++ IntergrapheFagique/main.cpp
@@ -13,3 +13,13 @@ int main(int argc, char** argv)
 	
 	return app.exec();
 }
+
+/*
+
+	TODO tout courre
+	Bouton supprimer
+	ajuster les espaces/alignements des widgets ajoutés
+	fiche de voeux de M1
+	terminer le bouton validation
+	faire un onglet aide dans le menu pour connaître la structure d'un triplet et tout ça
+*/

b30dce5c4b123ec26fec5b781915beece3279229
PierreGranier <piergranier@laposte.net>
2016-05-18 12:00:47 +0200

    Spacing martgin de regle a 0

--- IntergrapheFagique/widgetregle.cpp
+++ IntergrapheFagique/widgetregle.cpp
@@ -9,6 +9,9 @@ WidgetRegle::WidgetRegle(const string &nomRegle, QWidget *parent) : QWidget(pare
 	m_nomRegle = new WidgetNomRegle(nomRegle, this);
 	
 	m_grid->addWidget(m_nomRegle, 2, 0, 1, 2, Qt::AlignHCenter);
+	
+	m_grid->setSpacing(0);
+	m_grid->setContentsMargins(0, 0, 0, 0);
 }
 
 WidgetRegle::~WidgetRegle()

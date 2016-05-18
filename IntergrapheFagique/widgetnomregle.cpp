5396b6372441b6b2ff3a2aa6083c038fa7d0ba9b
PierreGranier <piergranier@laposte.net>
2016-05-18 16:02:30 +0200

    Alignements texte QImage terminé

--- IntergrapheFagique/widgetnomregle.cpp
+++ IntergrapheFagique/widgetnomregle.cpp
@@ -20,7 +20,6 @@ WidgetNomRegle::WidgetNomRegle(const string &nomRegle, QWidget *parent) : QWidge
 
 	// m_bufferPainter->drawLine(0, 20, this->width()/2-m_nomRegle.size()*4, 20);
 	// m_bufferPainter->drawText(this->width()/2-m_nomRegle.size()*3-2, 24, m_nomRegle);
-	// m_bufferPainter->drawText(0, 20, m_nomRegle);
 	// m_bufferPainter->drawLine(m_nomRegle.size(), 20, parent->width(), 20);
 }
 
@@ -32,18 +31,19 @@ WidgetNomRegle::~WidgetNomRegle()
 void WidgetNomRegle::paintEvent(QPaintEvent *event)
 {
 	this->resize(m_parent->size());
-	cout << "width et height " << m_parent->width() << ", " << m_parent->height() << endl;
-	
-	m_buffer = new QImage(m_parent->width(), 30, QImage::Format_RGB32);
-	m_buffer->fill(Qt::white);
-	
-	m_bufferPainter = new QPainter(m_buffer);
-	m_bufferPainter->setPen(Qt::black);
-	m_bufferPainter->setRenderHint(QPainter::Antialiasing, true);
-	
-	m_bufferPainter->drawText(0, 20, m_nomRegle);
-	m_bufferPainter->drawLine(m_nomRegle.size()*5, 20, m_parent->width(), 20);
-	
-	QPainter p(this);
-	p.drawImage(0, 0, *m_buffer);
+	if(m_nomRegle.toStdString() != "") 
+	{
+		m_buffer = new QImage(m_parent->width(), 30, QImage::Format_RGB32);
+		m_buffer->fill(Qt::white);
+		
+		m_bufferPainter = new QPainter(m_buffer);
+		m_bufferPainter->setPen(Qt::black);
+		m_bufferPainter->setRenderHint(QPainter::Antialiasing, true);
+		
+		m_bufferPainter->drawLine(0, 20, this->width()/2-m_nomRegle.size()*4, 20);
+		m_bufferPainter->drawText(this->width()/2+2-m_nomRegle.size()*3.5, 24, m_nomRegle);
+		m_bufferPainter->drawLine(this->width()/2+2+m_nomRegle.size()*6, 20, m_parent->width(), 20);
+		QPainter p(this);
+		p.drawImage(0, 0, *m_buffer);
+	}
 }
